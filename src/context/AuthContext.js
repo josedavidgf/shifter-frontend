8b6a110f887612d05204d0b1c97872d0fc233740
josedavidgf <josedavidgarciafernandez@gmail.com>
2025-04-07 22:40:50 +0100

    Fix AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { loginUser, registerUser } from '../services/authService';
 
 const AuthContext = createContext();
@@ -10,13 +10,22 @@ export function useAuth() {
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState(null);
 
+    // Cargar el usuario desde el localStorage al iniciar la aplicación
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setCurrentUser({ token });
+        }
+    }, []);
+
+    // Registro de usuario
     async function register(email, password) {
         try {
             const data = await registerUser(email, password);
             const accessToken = data.data.session.access_token;
-    
+
             if (!accessToken) throw new Error('Token no encontrado');
-    
+
             localStorage.setItem('token', accessToken);
             setCurrentUser({ token: accessToken });
             return data;
@@ -25,13 +34,14 @@ export function AuthProvider({ children }) {
         }
     }
 
+    // Inicio de sesión
     async function login(email, password) {
         try {
             const data = await loginUser(email, password);
             const accessToken = data.data.session.access_token;
-    
+
             if (!accessToken) throw new Error('Token no encontrado');
-    
+
             localStorage.setItem('token', accessToken);
             setCurrentUser({ token: accessToken });
             return data;
@@ -40,6 +50,7 @@ export function AuthProvider({ children }) {
         }
     }
 
+    // Cerrar sesión
     function logout() {
         localStorage.removeItem('token');
         setCurrentUser(null);
