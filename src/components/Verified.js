ea5f4ae9523182f5770031306be9f1ce7564e2b2
josedavidgf <josedavidgarciafernandez@gmail.com>
2025-04-14 11:54:22 +0100

    Update verfied page

--- src/components/Verified.js
+++ src/components/Verified.js
@@ -1,20 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Verified = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    localStorage.removeItem('token'); // Limpia el token manual
-    localStorage.removeItem('sb-xxqfqprwincmfwxmsjkf-auth-token'); // Limpia el usuario manual
-  }, []);
-
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
       <h2>✅ Email verificado correctamente</h2>
-      <p>Ya puedes iniciar sesión con tu cuenta.</p>
-      <button onClick={() => navigate('/login')} style={{ marginTop: '1rem' }}>
-        Ir al login
+      <p>Ya puedes continuar con tu onboarding.</p>
+      <button onClick={() => navigate('/onboarding')} style={{ marginTop: '1rem' }}>
+        Continua tu onboarding
       </button>
     </div>
   );
