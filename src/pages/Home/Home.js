d50885e863a7158a2f1000145c5dffdc69ac2f72
josedavidgf <josedavidgarciafernandez@gmail.com>
2025-05-09 13:00:32 +0100

    fix home css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,34 +8,34 @@ function Home() {
     const navigate = useNavigate();
 
     return (
-        <div className="container home-container">
-            <div className="home-content">
-                <div className="home-logo-container">
-                    <img src={logoTanda} alt="Tanda Logo" className="home-logo" />
-                </div>
+        <div className='page'>
+            <div className="container home-container">
+                <div className="home-content">
+                    <div className="home-logo-container">
+                        <img src={logoTanda} alt="Tanda Logo" className="home-logo" />
+                    </div>
 
-                <div className="home-hero">
-  <img src={illustration} alt="Ilustración" className="home-illustration" />
-  <p className="home-description">
-    Cambia tus turnos sin perder horas. Publica los que no quieres y consigue los que necesitas
-  </p>
-</div>
+                    <div className="home-hero">
+                        <img src={illustration} alt="Ilustración" className="home-illustration" />
+                        <p className="home-description">
+                            Cambia tus turnos sin perder horas. Publica los que no quieres y consigue los que necesitas
+                        </p>
+                    </div>
 
-                <div className="home-body">
-                    <div className="home-form">
-                        <div className="btn-group">
-                            <Button
-                                label="Iniciar sesión"
-                                variant="primary"
-                                size="lg"
-                                onClick={() => navigate('/login')}
-                            />
-                            <Button
-                                label="Registrarme"
-                                variant="secondary"
-                                size="lg"
-                                onClick={() => navigate('/register')}
-                            />
+                    <div className="home-body">
+                            <div className="btn-group btn-sticky-footer">
+                                <Button
+                                    label="Iniciar sesión"
+                                    variant="primary"
+                                    size="lg"
+                                    onClick={() => navigate('/login')}
+                                />
+                                <Button
+                                    label="Registrarme"
+                                    variant="secondary"
+                                    size="lg"
+                                    onClick={() => navigate('/register')}
+                                />
                         </div>
                     </div>
                 </div>
