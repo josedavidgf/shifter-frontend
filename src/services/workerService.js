e0e1e4c4b63f2a64e5691d2b22900b36aae87201
josedavidgf <josedavidgarciafernandez@gmail.com>
2025-05-11 16:10:50 +0100

    Update logs

--- src/services/workerService.js
+++ src/services/workerService.js
@@ -39,6 +39,9 @@ export const createWorker = async (data, token) => {
 export const getMyWorkerProfile = async (token) => {
   try {
     console.log('[DEBUG] Token:', token?.slice?.(0, 12));
+    console.log('[DEBUG] Entorno:', process.env.REACT_APP_ENV);
+    console.log('[DEBUG] Backend URL:', process.env.REACT_APP_BACKEND_URL);
+    console.log('[DEBUG] URL:', `${API_URL}/api/workers/me`);
     const response = await axios.get(`${API_URL}/api/workers/me`, authHeaders(token));
     return response.data.data;
   } catch (error) {
